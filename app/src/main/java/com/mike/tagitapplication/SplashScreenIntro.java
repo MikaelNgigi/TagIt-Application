0cafa94e6c18a5ee3c2dbdec0bd05e20fc822908
Michael Macharia Ngigi <michael.ngigi@strathmore.edu>
2021-07-19 12:40:59 +0300

    Created Fragment Java classes and implemented the onCreate View

--- app/src/main/java/com/mike/tagitapplication/SplashScreenIntro.java
+++ app/src/main/java/com/mike/tagitapplication/SplashScreenIntro.java
@@ -1,15 +1,29 @@
 package com.mike.tagitapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 import android.widget.ImageView;
 
 import com.airbnb.lottie.LottieAnimationView;
+import com.mike.tagitapplication.Fragments.OnBoardingFragment1;
+import com.mike.tagitapplication.Fragments.OnBoardingFragment2;
+import com.mike.tagitapplication.Fragments.OnBoardingFragment3;
+
+import org.jetbrains.annotations.NotNull;
 
 public class SplashScreenIntro extends AppCompatActivity {
     ImageView logo;
     LottieAnimationView lottieAnimationView;
+
+    private static final int NUM_PAGES = 3;
+    private ViewPager viewPager;
+    private ScreenSlidePagerAdapter pagerAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,7 +33,43 @@ public class SplashScreenIntro extends AppCompatActivity {
         logo = findViewById(R.id.tag_it_logo);
         lottieAnimationView = findViewById(R.id.target_splash);
 
+        viewPager = findViewById(R.id.liquid_pager);
+        pagerAdapter = new ScreenSlidePagerAdapter(getSupportFragmentManager());
+        viewPager.setAdapter(pagerAdapter);
+
         logo.animate().translationY(-1400).setDuration(1000).setStartDelay(4000);
         lottieAnimationView.animate().translationY(1600).setDuration(500).setStartDelay(4000);
+    }
+
+    private class ScreenSlidePagerAdapter extends FragmentStatePagerAdapter{
+
+        public ScreenSlidePagerAdapter(@NonNull @NotNull FragmentManager fm) {
+            super(fm);
+        }
+
+        @NonNull
+        @NotNull
+        @Override
+        public Fragment getItem(int position) {
+            switch (position){
+                case 0:
+                    OnBoardingFragment1 tab1 = new OnBoardingFragment1();
+                    return tab1;
+
+                case 1:
+                    OnBoardingFragment2 tab2 = new OnBoardingFragment2();
+                    return tab2;
+
+                case 2:
+                    OnBoardingFragment3 tab3 = new OnBoardingFragment3();
+                    return tab3;
+            }
+            return null;
+        }
+
+        @Override
+        public int getCount() {
+            return NUM_PAGES;
+        }
     }
 }
